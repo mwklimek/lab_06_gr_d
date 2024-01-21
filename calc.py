271f66b0f4fef835b68d4fe419fa5e6f2b7270ab
mwklimek <mwklimek@wsb-nlu.edu.pl>
2024-01-21 15:08:38 +0100

    kalkulator

--- calc.py
+++ calc.py
@@ -1,1 +1,25 @@
 # kalkulator
+
+import sys
+
+if len(sys.argv) != 4:
+	print("Uzycie: calc.py <num1> < + - > <num2>")
+	sys.exit(1)
+
+num1 = int(sys.argv[1])
+op = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if op == "+":
+	result = num1 + num2
+elif op == "-":
+	result = num1 - num2
+else:
+	print("Zly operator")
+	sys.exit(1)
+
+with open("/tmp/wynik.txt", "w") as file:
+	file.write( str(result) )
+
+print( result )
+
